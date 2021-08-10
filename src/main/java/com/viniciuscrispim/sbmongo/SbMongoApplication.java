b35b582a90189c5415dd6a0c93f70889203e004e
Vinicius Crispim <vinicrispim02@hotmail.com>
2021-08-10 18:24:51 -0300

    Projeto criado

---
+++ src/main/java/com/viniciuscrispim/sbmongo/SbMongoApplication.java
@@ -0,0 +1,13 @@
+package com.viniciuscrispim.sbmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciuscrispim/sbmongo/SbMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciuscrispim.sbmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
