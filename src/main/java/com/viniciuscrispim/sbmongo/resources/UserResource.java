1b7fa4658c1bc8eba513e21d67406b99df51fa49
Vinicius Crispim <vinicrispim02@hotmail.com>
2021-08-13 21:08:55 -0300

    Crud do User

--- src/main/java/com/viniciuscrispim/sbmongo/resources/UserResource.java
+++ src/main/java/com/viniciuscrispim/sbmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.viniciuscrispim.sbmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.viniciuscrispim.sbmongo.domain.User;
 import com.viniciuscrispim.sbmongo.dto.UserDTO;
@@ -40,5 +43,30 @@ public class UserResource {
 		UserDTO udto = new UserDTO(u);
 		return ResponseEntity.ok().body(udto);
 	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> saveUser(@RequestBody UserDTO udto){
+		User u = service.fromDTO(udto);
+		u = service.saveUser(u);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(u.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();			
+	}
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deleteUser(@PathVariable String id){
+		service.deleteUser(id);
+		return ResponseEntity.noContent().build();
+	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<UserDTO> updateUser(@PathVariable String id, @RequestBody UserDTO udto){
+		User u = service.fromDTO(udto);
+		u.setId(id);
+		service.updateUser(u);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- src/main/java/com/viniciuscrispim/sbmongo/services/UserService.java
+++ src/main/java/com/viniciuscrispim/sbmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.viniciuscrispim.sbmongo.domain.User;
+import com.viniciuscrispim.sbmongo.dto.UserDTO;
 import com.viniciuscrispim.sbmongo.repositories.UserRepository;
 import com.viniciuscrispim.sbmongo.services.exception.ObjectNotFoundException;
 
@@ -15,6 +16,7 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
+	private User u;
 	
 	public List<User> findAll(){
 		return repository.findAll();
@@ -23,5 +25,30 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> u = repository.findById(id);
 		return u.orElseThrow(() -> new ObjectNotFoundException("Usuario não encontrado"));
+	}
+	
+	public User saveUser(User u) {
+		return repository.insert(u);
+	}
+	
+	public void deleteUser(String id) {
+		findById(id); //caso o id nao exista ele lançara o erro
+		repository.deleteById(id);
+	}
+	
+	public User updateUser(User user) {
+		User u = findById(user.getId());
+		updateData(u, user);
+		return repository.save(u);
+	}
+	
+	private void updateData(User u, User user) {
+		u.setEmail(user.getEmail());
+		u.setName(user.getName());
+	}
+	
+	//converte UserDTO em User
+	public User fromDTO(UserDTO udto) {
+		return new User(udto.getId(),udto.getName(),udto.getEmail());
 	}
 }
