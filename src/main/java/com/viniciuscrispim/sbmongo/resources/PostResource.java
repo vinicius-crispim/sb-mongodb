bdb018254215b8f1481918688fecb3d81420ef24
Vinicius Crispim <vinicrispim02@hotmail.com>
2021-08-13 22:52:43 -0300

    Implementando PostService e PostResource

---
+++ src/main/java/com/viniciuscrispim/sbmongo/resources/PostResource.java
@@ -0,0 +1,53 @@
+package com.viniciuscrispim.sbmongo.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.viniciuscrispim.sbmongo.domain.Post;
+import com.viniciuscrispim.sbmongo.services.PostService;
+
+@RestController
+@RequestMapping (value = "/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService service;
+	
+	@RequestMapping(method =RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAll(){
+		List<Post> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post post = service.findById(id);
+		return ResponseEntity.ok().body(post);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> savePost(@RequestBody Post post){
+		post = service.savePost(post);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(post.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();			
+	}
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deletePost(@PathVariable String id){
+		service.deletePost(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+}
---
+++ src/main/java/com/viniciuscrispim/sbmongo/services/PostService.java
@@ -0,0 +1,38 @@
+package com.viniciuscrispim.sbmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.viniciuscrispim.sbmongo.domain.Post;
+
+import com.viniciuscrispim.sbmongo.repositories.PostRepository;
+import com.viniciuscrispim.sbmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public List<Post> findAll(){
+		return repository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> u = repository.findById(id);
+		return u.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	
+	public Post savePost(Post u) {
+		return repository.insert(u);
+	}
+	
+	public void deletePost(String id) {
+		findById(id); //caso o id nao exista ele lançara o erro
+		repository.deleteById(id);
+	}
+	
+}
