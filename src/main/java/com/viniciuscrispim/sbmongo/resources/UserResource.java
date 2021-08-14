9c01c98b88b37b63707f2b8b14bde0973be62f96
Vinicius Crispim <vinicrispim02@hotmail.com>
2021-08-13 22:38:28 -0300

    Endpoint para retornar os posts do User

--- src/main/java/com/viniciuscrispim/sbmongo/resources/UserResource.java
+++ src/main/java/com/viniciuscrispim/sbmongo/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.viniciuscrispim.sbmongo.domain.Post;
 import com.viniciuscrispim.sbmongo.domain.User;
 import com.viniciuscrispim.sbmongo.dto.UserDTO;
 import com.viniciuscrispim.sbmongo.services.UserService;
@@ -67,6 +68,12 @@ public class UserResource {
 		u.setId(id);
 		service.updateUser(u);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User u = service.findById(id);
+		return ResponseEntity.ok().body(u.getPosts());
 	}
 	
 }
